22243ca1256e6b2056c4dfa62ebe7ffe5b7f686b
Gabriel Manzini <gabriel.manzini@apiconsulting.com.br>
2019-02-24 06:30:19 -0300

    Adicionar arquivos de projeto.

---
+++ Api/Controllers/StockController.cs
@@ -0,0 +1,58 @@
+﻿using Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class StockController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<StockModel> Get()
+        {
+            var list = new List<StockModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new StockModel
+                {
+                    Id = i.ToString(),
+                    Type = "Rice",
+                    ExpirationDate = "1" + i.ToString() + "-02-2019",
+                    ExpirationOpened = "3"
+                });
+
+
+            }
+
+            return list;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]StockModel value)
+        {
+
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        public void Patch(int id, [FromBody] StockModel value)
+        {
+
+        }
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Api/Models/StockModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Api.Models
+{
+    [DataContract]
+    public class StockModel
+    {
+        [DataMember(Name = "íd")]
+        public string Id { get; set; }
+        [DataMember(Name = "expirationdate")]
+        public string ExpirationDate { get; set; }
+        [DataMember(Name = "expirationopened")]
+        public string ExpirationOpened { get; set; }
+        [DataMember(Name = "type")]
+        public string Type { get; set; }
+
+        [DataMember(Name = "dateopened")]
+        public string DateOpened { get; set; }
+    }
+}
